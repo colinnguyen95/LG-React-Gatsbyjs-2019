517fed5a54d901c69588147994818a9e849a8e91
colinnguyen95 <colinnguyen@Colins-iMac.local>
2019-04-04 18:40:15 -0700

    added input fix

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -14,6 +14,7 @@ const Contact = props => (
             </div>
             
             <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+                <input type="hidden" name="form-name" value="contact-form" />
                 <p>
                     <input type="text" name="name" placeholder="Name" id="name" required/>
                 </p>
